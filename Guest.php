eb17f9a48145c24596cbc254386a01c599968286
Dominque22 <dom.frazier@yahoo.com>
2017-01-31 12:06:31 -0500

    Update Guest.php

--- Guest.php
+++ Guest.php
@@ -8,9 +8,9 @@
   <div class="form">
     <form class="guest-ages">
       <p class="message">Select your age range.</a></p>
-      <button>Child (under 18)</button>
-      <button>Young Adult (18-30)</button>
-      <button>Adult (over 30)</button>
+      <button>Young (under 18)</button>
+      <button>Adult (18-30)</button>
+      <button>Older Adult (over 30)</button>
     </form>
   </div>
 </div>
