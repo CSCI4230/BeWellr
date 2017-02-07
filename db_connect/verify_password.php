f29c0f8d0994608702b853395fdef67199098123
Justin Whitaker <justindanielwhitaker@gmail.com>
2017-02-07 13:24:45 -0500

    Change to be used by including rather than POSTing

--- db_connect/verify_password.php
+++ db_connect/verify_password.php
@@ -1,21 +1,16 @@
 <?php     
  
 /*
- * verify_password.php returns true if the password input matches the true password, and false otherwise
+ * verifyPassword returns true if the password input matches the true password, and false otherwise
  *
  * Parameters: 
- *      userID - the identifier of the user whose password is being verified
+ *      userid - the identifier of the user whose password is being verified
  *      password - the string being compared against the user's password
  */
- 
-include 'db_connect\db_config.php';
 
-// check for required post data
-if (isset($_POST['userID']) && isset($_POST['password'])) {
-    
-    // parse post data into variables
-    $userID = $_POST['userID'];
-    $password = $_POST['password'];
+include 'db_connect\db_config.php';
+ 
+function verifyPassword($user_id, $password){{
     
     // establish a connection to the database
     $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE );
@@ -26,7 +21,7 @@ if (isset($_POST['userID']) && isset($_POST['password'])) {
 	}	
 
     // retrieve the users hash
-    $sql = "SELECT saltedhash FROM User_Data WHERE user_id = $userID";
+    $sql = "SELECT saltedhash FROM User_Data WHERE user_id = $user_id";
     $hash = $mysqli->query($sql);
 
     // verify the password is correct
@@ -34,10 +29,5 @@ if (isset($_POST['userID']) && isset($_POST['password'])) {
 
     //Close the connection
     $mysqli->close();
-}
-else 
-{
-    // required field is missing
-    return false;
 }
 ?>
