e3decdd194f6ecf2d35a05178de0fa94a7134d5b
allofthebytes <magsinom10@students.ecu.edu>
2017-02-08 10:32:13 -0500

    login

--- db_connect/verify_password.php
+++ db_connect/verify_password.php
@@ -8,9 +8,9 @@
  *      password - the string being compared against the user's password
  */
 
-include 'db_connect\db_config.php';
+include 'db_connect/db_config.php';
  
-function verifyPassword($user_id, $password){{
+function verifyPassword($user_id, $password){
     
     // establish a connection to the database
     $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE );
@@ -23,9 +23,9 @@ function verifyPassword($user_id, $password){{
     // retrieve the users hash
     $sql = "SELECT saltedhash FROM User_Data WHERE user_id = $user_id";
     $hash = $mysqli->query($sql);
-
+    $row = $hash->fetch_assoc();
     // verify the password is correct
-    return password_verify($password, $hash);
+    return password_verify($password, $row['saltedhash']);
 
     //Close the connection
     $mysqli->close();
--- login.php
+++ login.php
@@ -1,9 +1,45 @@
 <?php
-	include 'header.php';
+  ini_set('display_errors',1); // for error checking
+  error_reporting(E_ALL);      // for error checking
+  session_start();
+	
+  include_once 'header.php';
+  require_once __DIR__ . '/db_connect/db_config.php';
+  include 'db_connect/verify_password.php';
+  
+  if ($_POST && !empty($_POST['email']) && !empty($_POST['password']) )
+  {
+      // connect to database
+      $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+      // check connection
+      if ($mysqli->connect_error)
+      {
+          die('Connection error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect-error);
+      }
+
+      $username = $_POST['email'];
+      $pwd = $_POST['password'];
+
+      $sql = "SELECT user_id FROM user_data WHERE email = '$username' ";
+      $try = $mysqli->query($sql);
+      
+      $row = $try->fetch_assoc();
+
+      if ($sql && verifyPassword($row['user_id'], $pwd))
+      {
+          $_SESSION['email'] = $_POST['email'];
+          header("Location: welcome.php");
+      }
+      else
+      {
+          echo "Invalid Email or Password";
+      }
+  }
 ?>
     
 <h1>Login</h1>
-    
+ 
 <div class="login-page">
   <div class="form" >
     <form class="register-form">
