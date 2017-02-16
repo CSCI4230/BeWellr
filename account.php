c10c58da05791cd412862fa9a001e226a0d1e258
Justin Whitaker <justindanielwhitaker@gmail.com>
2017-02-16 13:01:26 -0500

    Add ability to delete account

--- account.php
+++ account.php
@@ -4,8 +4,12 @@
  <body>
  <?php
 	include 'header.php';
-  include 'db_connect\db_config.php';
-
+    require_once __DIR__ . '/db_connect/db_config.php';
+    require_once __DIR__ . '/db_connect/verify_password.php';
+        require_once __DIR__ . '/db_connect/delete_account.php';
+    
+    ini_set('display_errors',1); // for error checking
+    error_reporting(E_ALL);      // for error checking
 
   //connect to MySQL
   $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
@@ -46,6 +50,21 @@
 
       mysqli_close($connection);
 
+    if (!empty($_POST['password']) )
+    {
+        $password = $_POST['password'];
+        $result = verifyPassword($email, $password);
+
+        if ($result)
+        {
+            deleteAccount($email);
+            header("Location: logout.php");
+        }
+        else
+        {
+            echo "Incorrect password.";
+        }
+    }
 
 
   ?>
@@ -63,8 +82,9 @@
     <div class="deleteContainer">
       <h3 class="accountInfo"><u>Delete Account</u></h3>
       <p id="deleteParagraph">This action is permanent; think twice before proceeding!</p>
-      <div class="deleteButton">
-        <button class="btn btn-danger" id="delete">Delete Account</button>
+      <form class="login-form" method = "post">
+        <input type="password" name = "password" placeholder="Password"/>
+      <button>Delete Account</button>
       </div>
       </div></div>
  </body>
