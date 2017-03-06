f4e3b3a5eb305f068f9817b5f6783050fb91150b
lquick94 <quickl14@students.ecu.edu>
2017-03-06 08:19:33 -0500

    Preassessment questions are grabbed from database table now. Signed-off-by: lquick94 <quickl14@students.ecu.edu>

--- survey.php
+++ survey.php
@@ -1,25 +1,28 @@
 <?php 
 include 'header.php';	
+include 'db_connect\db_config.php';
+$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+
 ?>
 <div class="container2">
 <?php
-	$fileName;
+	$type;
+	
 	if (isset($_GET['youngAdult']) && empty ($_GET['youngAdult'])) {
-		$fileName = "young_adult_survey_questions.txt";
+		$type = "YA";
 	}
 	
 	else if (isset($_GET['Adult']) && empty ($_GET['Adult'])) {
-		$fileName = "adult_survey.txt";
+		$type = "A";
 	}
 	
 	else if (isset($_GET['olderAdult']) && empty ($_GET['olderAdult'])) {
-		$fileName = "young_adult_survey_questions.txt";
+		$type = "OA";
 	}
-	
-	$names=file($fileName);
-	foreach($names as $name)
-	{?>
-				<h3 class="border"><?php echo $name?></h3>
+	$query = mysqli_query($connection, "SELECT question from preassessment where $type = 1");
+	while ($row = mysqli_fetch_assoc($query)) {
+	?>
+				<h3 class="border"><?php echo $row["question"]?></h3>
 				<form>
 				<div class="qPadding" id="q1Bullets">
         <label class="border2"><input type="radio" name="hello">Never</label>                 
@@ -37,5 +40,5 @@ include 'header.php';
     <form class="survey" action="#" method="post">
         <button>Submit</button>
     </form>
- </div>
+		</div>
 
