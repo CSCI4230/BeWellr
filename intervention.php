c62aeda77d3007253d5ec86feee581386bd068a5
DESKTOP-C1PQH24\patin <patino37@yahoo.com>
2017-03-06 22:33:57 -0500

    Added an intervention.php file that will hold our intervention questions and averages. Content will be added at a later time.

---
+++ intervention.php
@@ -0,0 +1,42 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">    
+  <?php
+   include 'header.php';
+   logged_in_redirect();
+  ?>
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <script src="interventionScript.js"></script>
+
+    <div class="interventionMain">
+        <div class="tab">
+            <a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'London')">Wellness Questions</a>
+            <a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Paris')">Week Averages</a>
+            <a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Tokyo')">Overall Averages</a>
+        </div>
+
+        <div id="London" class="tabcontent">
+            <h3>London</h3>
+            <p>London is the capital city of England.</p>
+        </div>
+
+        <div id="Paris" class="tabcontent">
+            <h3>Paris</h3>
+            <p>Paris is the capital of France.</p>
+        </div>
+
+        <div id="Tokyo" class="tabcontent">
+            <h3>Tokyo</h3>
+            <p>Tokyo is the capital of Japan.</p>
+        </div>
+  </div>
+
+</body>
+</html>
