d65e00f31af71560dd9c84c6ea2566200e6bb5b1
DESKTOP-C1PQH24\patin <patino37@yahoo.com>
2017-03-07 06:38:49 -0500

    Added coments and minor bug fixes to intervention.php and interventionScript.js

--- intervention.php
+++ intervention.php
@@ -17,30 +17,45 @@
 
     <div class="interventionMain">
         <div class="tab">
-            <a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">Wellness Questions</a>
-            <a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Paris')">Week Averages</a>
-            <a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Tokyo')">Overall Averages</a>
+            <a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'wellnessQuestions')" id="defaultOpen">Wellness Questions</a>
+            <a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'weekAverages')">Week Averages</a>
+            <a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'overallAverages')">Overall Averages</a>
         </div>
 
-        <div id="London" class="tabcontent">
+        <div id="wellnessQuestions" class="tabcontent">
+            <!--Code for Questions go HERE-->
+            <!--Code for Questions go HERE-->
+            <!--Code for Questions go HERE-->
+            <!--Code for Questions go HERE-->
+            <!--Code for Questions go HERE-->
             <h3>London</h3>
             <p>London is the capital city of England.</p>
         </div>
 
-        <div id="Paris" class="tabcontent">
+        <div id="weekAverages" class="tabcontent">
+            <!--Code for Week Averages go HERE-->
+            <!--Code for Week Averages go HERE-->
+            <!--Code for Week Averages go HERE-->
+            <!--Code for Week Averages go HERE-->
+            <!--Code for Week Averages go HERE-->
             <h3>Paris</h3>
             <p>Paris is the capital of France.</p>
         </div>
 
-        <div id="Tokyo" class="tabcontent">
+        <div id="overallAverages" class="tabcontent">
+            <!--Code for Overall Averages go HERE-->
+            <!--Code for Overall Averages go HERE-->
+            <!--Code for Overall Averages go HERE-->
+            <!--Code for Overall Averages go HERE-->
+            <!--Code for Overall Averages go HERE-->
             <h3>Tokyo</h3>
             <p>Tokyo is the capital of Japan.</p>
         </div>
         
         <script>
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
-    </script>
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+        </script>
   </div>
 
 </body>
