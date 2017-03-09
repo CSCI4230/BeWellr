a91112db52087920130b701266de4c4f04e47cd0
Benjamin Clark <clarkb15@students.ecu.edu>
2017-03-08 22:36:23 -0500

    Added a mysql query to get the user_id for the user.  Added variables and info for prepping data storage from the intervention

--- intervention.php
+++ intervention.php
@@ -5,7 +5,15 @@
   <meta charset="utf-8">    
   <?php
    include 'header.php';
+   include 'db_connect/db_config.php';
    protect_page();
+   $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+   //Code for getting the user_id
+   $logged_in = $_SESSION['email'];
+   $query = "SELECT user_id FROM user_data WHERE email = '$logged_in'";
+   $result = mysqli_query($connection, $query);
+   $user_idArray = mysqli_fetch_array($result);
+   $user_id = $user_idArray['user_id'];
   ?>
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -32,49 +40,65 @@
     <?php
     $fileName = "coping.txt";
 	$names=file($fileName);
+	$copingArray; //creates an array for holding Coping answer values
+	$copingQCount = 0; //Counts the coping questions
 	foreach($names as $name)
 	{?>
 				<h6 class="border"><?php echo $name?></h6>
 				<form>
 				<div class="qPadding" id="q1Bullets">
-        <label class="border3"><input type="radio" name="Yes">Yes</label>                 
-        <label class="border3"><input type="radio" name="No">No</label> 
+        <label class="border3"><input type="radio" name="Yes" value=1>Yes</label>                 
+        <label class="border3"><input type="radio" name="No" value=0>No</label> 
       </div>
       </form>
+	  
     
-<?php }
+<?php 
+	$copingQCount++;
+	}
+
 ?>
     <h3>Food</h3>
     <?php
     $fileName = "food.txt";
 	$names=file($fileName);
+	$foodArray; //create an array for holding Food answer values
+	$foodQCount = 0; //counts the food questions
 	foreach($names as $name)
 	{?>
 				<h6 class="border"><?php echo $name?></h6>
 				<form>
 				<div class="qPadding" id="q1Bullets">
-        <label class="border3"><input type="radio" name="Yes">Yes</label>                 
-        <label class="border3"><input type="radio" name="No">No</label> 
+        <label class="border3"><input type="radio" name="Yes" value=1>Yes</label>                 
+        <label class="border3"><input type="radio" name="No" value=0>No</label> 
       </div>
       </form>
     
-<?php }
+<?php
+	$foodQCount++;
+	}
+	
 ?>
         <h3>Physical Activity</h3>
     <?php
     $fileName = "physical.txt";
 	$names=file($fileName);
+	$physicalArray; //creates an array for holding physical activity answer values
+	$physicalQCount = 0; //counts the physical activity questions
 	foreach($names as $name)
 	{?>
 				<h6 class="border"><?php echo $name?></h6>
 				<form>
 				<div class="qPadding" id="q1Bullets">
-        <label class="border3"><input type="radio" name="Yes">Yes</label>                 
-        <label class="border3"><input type="radio" name="No">No</label> 
+        <label class="border3"><input type="radio" name="Yes" value=1>Yes</label>                 
+        <label class="border3"><input type="radio" name="No" value=0>No</label> 
       </div>
       </form>
     
-<?php }
+<?php
+	$physicalQCount++;
+	}
+	
 ?>
     
     <form class="survey" action="#" method="post">
