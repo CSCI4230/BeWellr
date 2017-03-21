91ae104f2abb868946ad91f0aea285a21608595a
Dominque22 <dom.frazier@yahoo.com>
2017-03-21 10:28:37 -0400

    Update updateDb.php

--- accountsetting/updateDb.php
+++ accountsetting/updateDb.php
@@ -20,41 +20,26 @@ include 'db_connect/db_config.php';
 
 	};
 	
-	echo "<table border=1>"
-	<tr>
-	<th>Topic</th>
-	<th><Name</th>
-	<th>Attendance</th>
-	</tr>;
 	//Execute the query
 	//if(mysqli_query($con,$sql))
 		//header("refresh:2; url=account.php");
 	//else
 		//	echo "Not Updated";
 ?>
-while($record = mysql_fetch_array($myData)){
-	echo "<form action=updata.php method=post>";
-	echo "<tr>";
-	echo "<td>"; . "<input type=text name=email=" . $record['email'] . "</td>";
-	echo "<td>"; . "<input type=text name=First Name=" . $record['first'] . "</td>";
-	echo "<td>"; . "<input type=text name=Last Name=" . $record['last'] . "</td>";
-	echo "<td>"; . "<input type=text name=Date of Birth" . $record['birth'] . "</td>";
-	echo "<td>"; . "<input type=text name=Weight number=" . $record['weight'] . " </td>";
-	echo "<td>"; . "<input type=text name=Height number=" . $record['height'] . " </td>";
-	echo "<td>"; . "<input type=text name=Sex=" . $record['gender'] . " </td>";
-	echo "</form>";
-	
-?>	
-
 	<h1>Account Update</h1>
-	
     <div class="Update-page">
         <div class="form">
             <form method="post" action="">
 			<p>Update First name</p>
-			 <input name="email" type="email" placeholder="email" required />
-			<input name="newname" type="text" placeholder="new name" required />
-			<button>Reset</button>
+			<input name="email" type="email" placeholder="email" required />
+			<input name="newname" type="text" placeholder="Update First name" required />
+			<input name="email" type="email" placeholder="Update email" required />
+			<input name="newname" type="text" placeholder="Update Last name" required />
+			<input name="email" type="email" placeholder="Update Email" required />
+			<input name="newname" type="text" placeholder="Update Weight" required />
+			<input name="email" type="email" placeholder="Update Height" required />
+			<input name="newname" type="text" placeholder="Update Gender" required />
+			<button>Update</button>
 			</form>
         </div>
     </div>
