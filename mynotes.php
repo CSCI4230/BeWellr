cb8bda64210f47f9018be42040abca9e20ed0144
DESKTOP-C1PQH24\patin <patino37@yahoo.com>
2017-04-11 18:47:09 -0400

    Added somewhat of a note display section.

--- mynotes.php
+++ mynotes.php
@@ -6,47 +6,48 @@
     include 'header.php';
       require_once __DIR__ . '/db_connect/db_config.php';
 
-    // //connect to MySQL
-    // $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
-    //
-    // if(!$connection)
-    // {
-    //   die("Connection error: " .mysqli_connect_error());
-    // }
-    // else {
-    //   echo "";
-    // }
-    //
-    // //current logged in user
-    // $logged_in = $_SESSION['email'];
-    //
-    // //SQL query that pulls user information from the user_data table
-    // $query = "SELECT note, date FROM notes WHERE email = '$logged_in'";
-    //
-    // $result = mysqli_query($connection, $query);
-    //
-    //   if(mysqli_num_rows($result) > 0)
-    //   {
-    //       while($row = mysqli_fetch_array($result))
-    //       {
-    //         $note = $row["note"];
-    //         $date = $row["date"];
-    //       }
-    //   }
-    //   else
-    //   {
-    //       echo("No results");
-    //   }
-    //
-    //     mysqli_close($connection);
+    //connect to MySQL
+    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+    if(!$connection)
+    {
+      die("Connection error: " .mysqli_connect_error());
+    }
+    else {
+      echo "";
+    }
+
+    //current logged in user
+    $logged_in = $_SESSION['email'];
+
+    //SQL query that pulls user information from the user_data table
+    $query = "SELECT date, note FROM notes WHERE email = '$logged_in'";
+
+    $result = mysqli_query($connection, $query);
+
+      if(mysqli_num_rows($result) > 0)
+      {
+        echo '<div class="middleContainer">';
+        echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+        echo '<tr><th>Date</th><th>Note</th></tr>';
+
+          while($row = mysqli_fetch_array($result)) {
+        			echo '<tr>';
+        			foreach($row as $key=>$value) {
+        				echo '<td>',$value,'</td>';
+        			}
+        			echo '</tr>';
+        		}
+  		      echo '</table><br />';
+            echo '</div>';
+      }
+      else
+      {
+          echo("No results");
+      }
+
+        mysqli_close($connection);
 
     ?>
-  <div class="middleContainer">
-    <h3 class="accountInfo"><u>Notes</u></h3>
-    <div class="personalInfo">
-      <p>Date: </p>
-      <p>Note: </p>
-    </div>
-  </div>
  </body>
 </html>
